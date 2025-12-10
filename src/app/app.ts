f48dfff5c77f067ad975b6f09f16b3fc78af8b15
Michele Segata <michele.segata@zupit.it>
2025-12-10 09:17:36 +0100

    add user management features with title and user updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -9,4 +15,27 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('signal-test');
+
+protected readonly users = signal<User[]>([
+    { id: 1, name: 'John Doe', email: 'asd@asd.it'}
+      ]);
+
+  updateTitle() {
+    this.title.update(() => 'asdasdas');
+    this.users.update(users => [
+      ...users,
+      { id: 2, name: 'Jane Smith', email: 'asdsadas@.it'}
+    ]);
+  }
+
+  updateFirstUserName() {
+    this.users.update(users => {
+      if (users.length === 0) {
+        return users;
+      }
+      const updatedUsers = [...users];
+      updatedUsers[0] = {...updatedUsers[0], name: 'Updated Name'};
+      return updatedUsers;
+    });
+  }
 }
